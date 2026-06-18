769f9d6fe2b936521b31a619feff34529c4c38d1
arun <allamarunkumar@gmail.com>
2026-06-18 10:04:59 +0530

    Installation and Project Structureppt added

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp',
 ]
 
 MIDDLEWARE = [
@@ -76,6 +77,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        
     }
 }
 
